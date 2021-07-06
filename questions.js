e073a7e5c738adbcfe782212ec2ce1bd786fcccd
Arthur Oliveira <zompy@riseup.net>
2021-07-06 13:35:31 -0300

    first commit

---
+++ questions.js
@@ -0,0 +1,254 @@
+questions = [
+    {
+        "question": "Eu sei qual é o uso correto da crase.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eu votei no Lenarto em alguma das últimas três eleições",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eu apoio o Estado de Israel.",
+        "effect": {
+            "econ": -5,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eu votarei/já votei contrário a renúncia do Tokiyo.",
+        "effect": {
+            "econ": 5,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eu já discuti política ou religião no chat principal.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Eu já presidi um dos três poderes.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Eu tive o cargo de Utopia Ancap",
+        "effect": {
+            "econ": 15,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eu acho que Ciro Gomes seria um bom Presidente da República na vida real.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 15,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eu já discuti uma pauta no Congresso ao invés de só votá-la. (Marque neutro se nunca foi congressista)",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Eu sou de esquerda na vida real.",
+        "effect": {
+            "econ": 10,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Já votei em algo/alguém após o Luscas ter me requisitado no privado.",
+        "effect": {
+            "econ": -15,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Já disputei eleições pelo PSDB.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Já me filiei a um partido presidido pelo Ângelo.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Sou ou já fui moderador.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": -5,
+            "scty": -10
+        }
+    },
+    {
+        "question": "Já fui processado por infidelidade partidária.",
+        "effect": {
+            "econ": 5,
+            "dipl": -5,
+            "govt": 5,
+            "scty": -5
+        }
+    },
+    {
+        "question": "Eu nunca ganhei uma eleição para um cargo no executivo durante o Império..",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -15,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Privatizações são, em geral, necessárias e boas para o desenvolvimento.",
+        "effect": {
+            "econ": -10,
+            "dipl": 10,
+            "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Eu me considero etnica ou religiosamente um judeu.",
+        "effect": {
+            "econ": 0,
+            "dipl": 15,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você votou no Tokiyo em alguma eleição geral.",
+        "effect": {
+            "econ": 0,
+            "dipl": -15,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você já leu Keynes no original pelo menos uma vez.",
+        "effect": {
+            "econ": 0,
+            "dipl": 15,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você usa uma rosa no nick do Brasil.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 15,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você já foi do PL ou dos partidos de Ghost do Carichau.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 15,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você é amigo das pessoas do bunker b. ou pertence a ele.",
+        "effect": {
+            "econ": 15,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você é católico, mas não era judeu há menos de um ano.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você tem orgulho da sua nação",
+        "effect": {
+            "econ": 10,
+            "dipl": -10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você apoia a social-democracia.",
+        "effect": {
+            "econ": -15,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Você participou ativamente da Independência, da Proclamação da República ou da Restauração do Império.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -15
+        }
+    },
+    {
+        "question": "Você tem, pelo menos, nível 40 no servidor.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": -15
+        }
+    }
+];
